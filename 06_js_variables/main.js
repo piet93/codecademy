e44826f0b23d38a684433620210ab6e9c2d4d836
piet93 <61916935+piet93@users.noreply.github.com>
2020-03-26 14:24:12 +0100

    typeof operator

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,3 +1,4 @@
-const myName = 'Peter';
-const myCity = 'Innsbruck';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);
